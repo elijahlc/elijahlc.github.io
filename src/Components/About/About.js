f163b72c5f5641868a6cba67762db9038c4c16a7
Eli Cohen <cohen.elijahlev@gmail.com>
2023-02-10 14:55:20 -0500

    updated language

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,26 +4,24 @@ const About = () => {
 	return (
 		<Section>
 			<Heading renderAs="h2" textFamily="secondary" textTransform="uppercase" textSize={1} textColor="primary">
-				Hi, I'm Eli.
+				Hi there, I'm Eli.
 			</Heading>
-			<Content renderAs="p" textSize={4}>
-				In 2019, while working at an advertising agency, I solved a basic Javascript bug related to pixel tracking for a
-				client's Facebook marketing. My excitement was sparked, and I started learning Javascript on my own for 2 years
-				before enrolling in an immersive engineering program with Fullstack Academy to continue my learning in a
-				collaborative environment. As my engineering skills have deepened and my focus has expanded, solving problems
-				for others (end users, internal colleagues and teammates, or company clients) remains the central tenet of what
+			<Content renderAs="p" textSize={5}>
+				In 2019, while working at an advertising agency, I solved a basic bug related to a client's Facebook pixel. My
+				excitement for using my analytical skills to write and debug code was sparked, and I started learning Javascript
+				on my own for 2 years before enrolling in an immersive engineering program with Fullstack Academy to continue my
+				learning in a collaborative environment. As my engineering skills have deepened and my focus has expanded,
+				solving problems for others (both my teammates and the end users of a product) remains the central tenet of what
 				drives me as a software engineer.
 			</Content>
 
-			<Content renderAs="p" textSize={4}>
-				For the past 5 years, I held leadership roles in Customer Success and Client Services at multiple early-stage
-				advertising technology startups. In these roles, I worked cross-functionally with product and engineering teams
-				to convey user requirements, test and roll out new features of our products, and iterate based on user feedback
-				to ensure positive customer platform experiences. Above all, my experiences as a Customer Success leader have
-				led me to be an empathetic and user-focused software engineer.
+			<Content renderAs="p" textSize={5}>
+				I have brought my 5 years of Customer Success and Client Services leadership experience to my pivot into
+				software engineering, which has led me to be an empathetic and user-focused software engineer, enthusiastic
+				about collaborating to provide scalable solutions to user problems.
 			</Content>
 
-			<Content renderAs="p" textSize={4}>
+			<Content renderAs="p" textSize={5}>
 				To learn more about my background, check out my <a href="https://www.linkedin.com/in/elijahcohen/">LinkedIn</a>{' '}
 				and to see code I've contributed to, check out my <a href="https://github.com/elijahlc">Github</a>!
 			</Content>
