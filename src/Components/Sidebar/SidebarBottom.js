bc1ce0a839c7dbc35d3e984b78c98e6112bfb022
Eli Cohen <cohen.elijahlev@gmail.com>
2023-02-12 19:53:07 -0500

    centered icon spacing

--- src/Components/Sidebar/SidebarBottom.js
+++ src/Components/Sidebar/SidebarBottom.js
@@ -6,10 +6,10 @@ const SidebarBottom = () => {
 	return (
 		<div className="SidebarBottom">
 			<Content renderAs="p" textSize={2}>
-				<Icon renderAs="a" href="https://github.com/elijahlc" color="white-bis" pr={6}>
+				<Icon renderAs="a" href="https://github.com/elijahlc" color="white-bis" pr={5}>
 					<i className="fa-brands fa-github"></i>
 				</Icon>
-				<Icon renderAs="a" href="https://www.linkedin.com/in/elijahcohen/" color="white-bis">
+				<Icon renderAs="a" href="https://www.linkedin.com/in/elijahcohen/" color="white-bis" pl={5}>
 					<i className="fa-brands fa-linkedin"></i>
 				</Icon>
 			</Content>
